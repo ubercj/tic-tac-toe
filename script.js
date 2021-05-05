ae51a9a40b2cde88a9b08f7f4f12f8b9bbb2d139
ubercj <ubercj@gmail.com>
2021-05-04 20:50:06 -0400

    Fixed a bunch of errors and removed play loop

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // DOM elements
 const gameArea = document.querySelector(".game-area");
+const ticker = document.querySelector(".ticker");
+const startButton = document.querySelector(".start");
 
 // Module for Game Board
 
@@ -34,33 +36,63 @@ const DisplayController = (() => {
       gameArea.lastChild.remove();
     }
   }
-  
-  const displayBoard = (gameboard) => {
+
+  const resetBoard = () => {
+    Board.grid.forEach(row => {
+      row.forEach((square, index, row) => {
+        row[index] = " ";
+      });
+    })
     _clearBoard();
+  }
+
+  // const refreshBoard = () => {
+  //   Board.grid.forEach(row => {
+  //     row.forEach((square, index, row) => {
+  //       let y = Number(square.getAttribute("y-position"));
+  //       let x = Number(square.getAttribute("x-position"));
+  //       row[index] = " ";
+  //     });
+  //   })
+  // }
+  
+  const displayBoard = () => {
+    // _clearBoard();
     let i = 0;
-    gameboard.forEach(row => {
+    let j = 0;
+    Board.grid.forEach(row => {
       row.forEach(square => {
         let space = document.createElement("div");
         space.textContent = square;
         space.classList.add("space");
-        space.setAttribute("position", `${i}`);
+        space.setAttribute("x-position", `${i}`);
+        space.setAttribute("y-position", `${j}`);
         gameArea.appendChild(space);
         i++;
       })
+      i = 0;
+      j++;
     })
-    // TODO: Break this out into submethods
+
+    // TODO: Break this out into submethods?
     spaces = document.querySelectorAll(".space");
     spaces.forEach(space => {
       space.addEventListener("click", () => {
+        let y = Number(space.getAttribute("y-position"));
+        let x = Number(space.getAttribute("x-position"));
         if(!space.classList.contains("clicked")) {
-          space.textContent = "X";
           space.classList.add("clicked");
+          Board.grid[y][x] = currentPlayer.getSymbol();
+          space.textContent = currentPlayer.getSymbol();
+          switchPlayer();
+          ticker.textContent = `It's ${currentPlayer.getName()}'s turn.`;
         }
+        // DisplayController.refreshBoard();
       })
     })
   }
 
-    return { displayBoard };
+    return { displayBoard, resetBoard };
 })();
 
 // Factory function for Players
@@ -95,33 +127,43 @@ const Game = () => {
     return _winner() || _fullBoard();
   }
 
-  const play = () => {
-    while(true) {    
-      if(_gameOver()) {
-        // Win or draw message
-        console.log("Game is over!");
-        break;
-      }
-      // Display whose turn it is
-
-      // "Turn" function that waits for player to click on a square
-      //     and then fills the square in
-      let y = prompt("Y coordinate?");
-      let x = prompt("X coordinate?");
-      let mark = prompt("'X' or 'O'?");
-      Board.grid[y][x] = mark;
-      DisplayController.displayBoard(Board.grid);
-      // Switches player
+  const play = (player1, player2) => {
+    DisplayController.displayBoard();
+    // let currentPlayer = player1;
+    if(_gameOver()) {
+      console.log("Game is over!");
     }
+    // Display whose turn it is
+    ticker.textContent = `It's ${currentPlayer.getName()}'s turn.`;
+
+    // "Turn" function that waits for player to click on a square
+    //     and then fills the square in
+    // let y = prompt("Y coordinate?");
+    // let x = prompt("X coordinate?");
+    // let mark = prompt("'X' or 'O'?");
+    // Board.grid[y][x] = mark;
+    // Switches player
   }
 
   return { play };
 }
 
-// Game script
-
 playerX = Player("John", "X");
 playerY = Player("Jane", "O");
 
-// Can I collect the player names from DOM?
+// Global event listeners
+startButton.addEventListener("click", () => {
+  DisplayController.resetBoard();
+  let newGame = Game();
+  newGame.play(playerX, playerY);
+})
 
+let currentPlayer = playerX;
+
+const switchPlayer = () => {
+  if (currentPlayer == playerX) {
+    currentPlayer = playerY;
+  } else {
+    currentPlayer = playerX;
+  }
+}
